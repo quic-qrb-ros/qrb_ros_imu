94f75c45fd55fc16c38a4bc66198a897e17fdb97
Lin Zhanye <zhanlin@qti.qualcomm.com>
2024-09-23 10:32:17 +0800

    update cmake build & remove unuse header

--- qrb_sensor_client/src/sensor_client.cpp
+++ qrb_sensor_client/src/sensor_client.cpp
@@ -21,9 +21,6 @@
 #include <thread>
 #include <vector>
 
-#include "remote.h"
-#include "rpcmem.h"
-
 #define PACK_NUM_MAX (1000)  // ring buffer len
 
 #ifndef DIRECT_CHANNEL_SHARED_MEMORY_SIZE
@@ -39,7 +36,7 @@ int count_gyro = 0;   // gyro current read position
 int accel_gyro_offset = -1;
 bool is_accel_gyro_offset_inited = false;
 std::vector<int> sensors_fd;
-std::vector<char*> sensors_buffer_ptr;
+std::vector<char *> sensors_buffer_ptr;
 int msg_socket[1] = { -1 };
 int original_sample_rate = 240;
 int adjusted_sample_rate = 240;
@@ -47,9 +44,10 @@ int adjusted_sample_rate = 240;
 int get_sensors_fd(int socket_fd, int fd_number)
 {
   msghdr msg;
-  cmsghdr* cm;
-  int* fds = (int*)malloc(fd_number * sizeof(int));
-  char buf[CMSG_SPACE(fd_number * sizeof(int))], iov_buf[1];
+  cmsghdr * cm;
+  int * fds = (int *)malloc(fd_number * sizeof(int));
+  char iov_buf[1];
+  char buf[100];
   memset(buf, '\0', sizeof(buf));
 
   iovec iov[1];
@@ -61,7 +59,7 @@ int get_sensors_fd(int socket_fd, int fd_number)
   msg.msg_name = nullptr;
   msg.msg_namelen = 0;
 
-  msg.msg_control = buf;
+  msg.msg_control = &buf;
   msg.msg_controllen = sizeof(buf);
 
   if (recvmsg(socket_fd, &msg, 0) < 0) {
@@ -71,10 +69,11 @@ int get_sensors_fd(int socket_fd, int fd_number)
 
   cm = CMSG_FIRSTHDR(&msg);
 
-  memcpy(fds, (int*)CMSG_DATA(cm), fd_number * sizeof(int));
+  memcpy(fds, (int *)CMSG_DATA(cm), fd_number * sizeof(int));
   for (int i = 0; i < fd_number; i++) {
     sensors_fd.emplace_back(fds[i]);
   }
+  free(fds);
   return 0;
 }
 
@@ -86,8 +85,8 @@ int get_sensors_ptr(int client_fd)
   }
 
   for (int i = 0; i < (int)sensors_fd.size(); i++) {
-    char* temp = (char*)(void*)mmap(NULL, DIRECT_CHANNEL_SHARED_MEMORY_SIZE, PROT_READ, MAP_SHARED,
-                                    sensors_fd[i], 0);
+    char * temp = (char *)(void *)mmap(
+        NULL, DIRECT_CHANNEL_SHARED_MEMORY_SIZE, PROT_READ, MAP_SHARED, sensors_fd[i], 0);
     if (temp == nullptr) {
       std::cout << "sensors_buffer_ptr is nullptr!" << std::endl;
       return -1;
@@ -109,7 +108,7 @@ bool SensorClient::CreateConnection()
   server_addr.sun_family = AF_UNIX;
   strcpy(server_addr.sun_path, SOCKET_PATH);
 
-  int ret = connect(_client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  int ret = connect(_client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
   if (ret < 0) {
     std::cout << "please check if the sensor service 's config is right." << std::endl;
     std::cout << "or please check if the sensor service is running." << std::endl;
@@ -142,7 +141,7 @@ bool SensorClient::CreateConnection()
  * index: last read position, range: [0, PACK_NUM_MAX-1].
  * last_count: last sensor data count. The newer sensor data count should bigger than it.
  */
-int get_available_sensors_nums(char* sensor, int index, int last_count)
+int get_available_sensors_nums(char * sensor, int index, int last_count)
 {
   int count = 0;
   // if index + 1 == PACK_NUM_MAX means come to the buffer bottom, need reset it to the buffer top.
@@ -151,7 +150,7 @@ int get_available_sensors_nums(char* sensor, int index, int last_count)
   }
 
   while (index + 1 < PACK_NUM_MAX) {
-    sensors_event_t* next_data = (sensors_event_t*)sensor + index + 1;
+    sensors_event_t * next_data = (sensors_event_t *)sensor + index + 1;
     if (next_data->reserved0 == 0 || next_data->reserved0 <= last_count) {
       break;
     }
@@ -161,13 +160,13 @@ int get_available_sensors_nums(char* sensor, int index, int last_count)
   return count;
 }
 
-int get_offset_of_data(char* sensor, int64_t target_timestamp)
+int get_offset_of_data(char * sensor, int64_t target_timestamp)
 {
   int index = 0;
   long ts_diff =
       1000000000L / adjusted_sample_rate / 2;  // 1s = 1000000000ns, rise and fall 1s / rate / 2
   while (index < PACK_NUM_MAX) {
-    sensors_event_t* next_data = (sensors_event_t*)sensor + index;
+    sensors_event_t * next_data = (sensors_event_t *)sensor + index;
     if (next_data->timestamp == 0 || (next_data->timestamp - ts_diff > target_timestamp)) {
       return -1;
     }
@@ -197,13 +196,14 @@ int get_offset_of_data(char* sensor, int64_t target_timestamp)
  *               999|         |                 999|         |
  *                  *---------*                    *---------*
  */
-bool SensorClient::GetImuData(sensors_event_t** accel_ptr, sensors_event_t** gyro_ptr,
-                              int32_t* sample_count)
+bool SensorClient::GetImuData(sensors_event_t ** accel_ptr,
+    sensors_event_t ** gyro_ptr,
+    int32_t * sample_count)
 {
   static int OFFSET =
       0;  // if found accel_gyro_offset, after each read need to move pointer to next.
-  sensors_event_t* data_accel = (sensors_event_t*)sensors_buffer_ptr[0] + count_accel;
-  sensors_event_t* data_gyro = (sensors_event_t*)sensors_buffer_ptr[1] + count_gyro;
+  sensors_event_t * data_accel = (sensors_event_t *)sensors_buffer_ptr[0] + count_accel;
+  sensors_event_t * data_gyro = (sensors_event_t *)sensors_buffer_ptr[1] + count_gyro;
 
   if (!is_accel_gyro_offset_inited) {
     int gyro_offset = get_offset_of_data(sensors_buffer_ptr[1], data_accel->timestamp);
@@ -238,8 +238,8 @@ bool SensorClient::GetImuData(sensors_event_t** accel_ptr, sensors_event_t** gyr
         accel_available_nums < gyro_available_nums ? accel_available_nums : gyro_available_nums;
   }
   // update return value
-  *accel_ptr = (sensors_event_t*)sensors_buffer_ptr[0] + ((count_accel + OFFSET) % PACK_NUM_MAX);
-  *gyro_ptr = (sensors_event_t*)sensors_buffer_ptr[1] + ((count_gyro + OFFSET) % PACK_NUM_MAX);
+  *accel_ptr = (sensors_event_t *)sensors_buffer_ptr[0] + ((count_accel + OFFSET) % PACK_NUM_MAX);
+  *gyro_ptr = (sensors_event_t *)sensors_buffer_ptr[1] + ((count_gyro + OFFSET) % PACK_NUM_MAX);
   // update current value
   count_accel = (count_accel + *sample_count) % PACK_NUM_MAX;
   count_gyro = (count_accel + accel_gyro_offset + PACK_NUM_MAX) % PACK_NUM_MAX;
@@ -263,7 +263,7 @@ void SensorClient::DisconnectServer()
   _client_fd = 0;
 }
 
-int SensorClient::ReadMsg(char* buffer, int len)
+int SensorClient::ReadMsg(char * buffer, int len)
 {
   if (_client_fd < 0)
     return -1;
